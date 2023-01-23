87c9d9329463962f48495accf199c0fb137239a6
MSherbinii <sherbini2002@gmail.com>
2023-01-24 01:08:43 +0200

    Updates

---
+++ Server/db.c
@@ -0,0 +1,10 @@
+#include "db.h"
+
+ST_accountsDB_t accountsDB[255] = {
+        {0,10000, RUNNING, "4550746475531533"},
+        {1,5000, BLOCKED, "4699094091899964"},
+        {2,1000, RUNNING, "4683982334261711"},
+        {3,20000, BLOCKED, "4684895000385128"},
+        {4,16000, RUNNING, "4315503473726102"}
+};
+ST_transaction_t transactionsDB[255] = {0};
---
+++ Server/db.h
@@ -0,0 +1,8 @@
+#ifndef DB_H
+#define DB_H
+#include "server.h"
+
+extern ST_accountsDB_t accountsDB[255];
+extern ST_transaction_t transactionsDB[255];
+
+#endif
